cd11e835158116cc6e0e2529008c3437c153442e
voste126 <steveaustine126@gmail.com>
2023-06-13 13:09:08 +0300

    introduction to arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (num1,num2) => {
+    return 2000/100;   
+}
+const square = (num3) => {
+    return num3 *= num3
+}
+const add = (a,b) => {
+    const add = a + b;
+    return add;
+}
